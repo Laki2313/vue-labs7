4d317a640b8b6c15b101ffdf9187f588836b4a86
Laki2313 <halych.ira@gmail.com>
2026-04-06 17:31:07 +0300

    Task 1: project scaffold + UI states

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,46 @@
 <template>
-  <div>
-    <NavBar />
-    <router-view />
+  <div class="app">
+    <h1>Каталог постів</h1>
+
+    <p v-if="isLoading">Завантаження...</p>
+
+    <p v-else-if="error">Помилка: {{ error }}</p>
+
+    <p v-else-if="items.length === 0">Немає даних</p>
+
+    <ul v-else>
+      <li v-for="item in items" :key="item.id">
+        {{ item.title }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import NavBar from './components/NavBar.vue'
+import { ref } from 'vue'
 
 export default {
-  components: {
-    NavBar
+  setup() {
+    const items = ref([])      
+    const isLoading = ref(false)
+    const error = ref(null)
+
+    return {
+      items,
+      isLoading,
+      error
+    }
   }
 }
 </script>
+
+<style>
+.app {
+  padding: 20px;
+  font-family: Arial;
+}
+
+h1 {
+  margin-bottom: 20px;
+}
+</style>
